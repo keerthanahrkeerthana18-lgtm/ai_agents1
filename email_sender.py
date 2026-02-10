e1b021679c8fb072417f9733e6de06269d549a6a
keerthanahrkeerthana18-lgtm <keerthanahrkeerthana18@gmail.com>
2026-02-10 10:36:21 +0530

    secrets1

--- email_sender.py
+++ email_sender.py
@@ -3,21 +3,21 @@ from email.message import EmailMessage
 from secrets import sender_email,receiver_email,app_password
 
 # Email details
-sender_email = "keerthanahrkeerthana18@gmail.com"
-receiver_email = "4mh23cs063@gmail.com"
-app_password = "xlyq lisg bpqh zyjp"
+def send_email(receiver_email,content):
 
 # Create email
-msg = EmailMessage()
-msg["From"] = sender_email
-msg["To"] = receiver_email
-msg["Subject"] = "Test Email from Python"
-msg.set_content("Hello!\nThis email was sent using Python.")
+    msg = EmailMessage()
+    msg["From"] = sender_email
+    msg["To"] = receiver_email
+    msg["Subject"] = "subject"
+    msg.set_content(content)
 
-# Connect to Gmail SMTP server
-with smtplib.SMTP("smtp.gmail.com", 587) as server:
-    server.starttls()                 # Secure connection
-    server.login(sender_email, app_password)
-    server.send_message(msg)
+    # Connect to Gmail SMTP server
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()                 # Secure connection
+        server.login(sender_email, app_password)
+        server.send_message(msg)
 
-print("Email sent successfully!")
+    print("Email sent successfully!")
+
+send_email(receiver_email="keerthanahrkeerthana18@gmail.com",subject="Hello from python",content="This is a email sent using python.")
